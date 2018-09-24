9fdd0600248745b04a909eb3863b7f2b4cf73904
Tamil <Tamilselvi.e2@gmail.com>
2018-09-23 22:47:03 -0700

    Revert "Generate String for field values"

--- onlinestore/src/main/java/com/techinterview/onlinestore/domain/BackPack.java
+++ onlinestore/src/main/java/com/techinterview/onlinestore/domain/BackPack.java
@@ -48,12 +48,4 @@ public class BackPack extends Product {
     public void setCapacity(double capacity) {
         this.capacity = capacity;
     }
-    @Override
-	public String toString() {
-		
-		return super.toString() 
-				+ ", " + "maxContentWeight: " + maxContentWeight 
-				+ ", " + "color: \"" + color + "\"" 
-				+ ", " + "capacity: " + capacity;
-	}
 }
